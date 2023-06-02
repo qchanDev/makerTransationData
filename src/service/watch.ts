0463f1665320608be5124f7603a760c06396885d
skywell <719779468@qq.com>
2023-06-03 00:18:56 +0800

    starknet

--- src/service/watch.ts
+++ src/service/watch.ts
@@ -41,6 +41,18 @@ export class Watch {
     }
   }
   public async start() {
+    const starknetHashFormat = (txHash: string) => {
+      if (txHash.length < 66) {
+        const end = txHash.substring(2, txHash.length);
+        const add = 64 - end.length;
+        let addStr = "";
+        for (let i = 0; i < add; i++) {
+          addStr += "0";
+        }
+        txHash = "0x" + addStr + end;
+      }
+      return txHash;
+    };
     const ctx = this.ctx;
     try {
       const chainGroup = groupWatchAddressByChain(ctx, ctx.makerConfigs);
@@ -61,19 +73,8 @@ export class Watch {
         ctx.logger.info(
           `Start Subscribe ChainId: ${id}, instanceId:${this.ctx.instanceId}, instances:${this.ctx.instanceCount}`,
         );
+
         pubSub.subscribe(`${id}:txlist`, async (txList: Transaction[]) => {
-          const starknetHashFormat = (txHash: string) => {
-            if (txHash.length < 66) {
-              const end = txHash.substring(2, txHash.length);
-              const add = 64 - end.length;
-              let addStr = "";
-              for (let i = 0; i < add; i++) {
-                addStr += "0";
-              }
-              txHash = "0x" + addStr + end;
-            }
-            return txHash;
-          };
           for (const tx of txList) {
             const chainId: any = tx.chainId;
             const hash = chainId == "SN_MAIN" ? starknetHashFormat(tx.hash) : tx.hash;
@@ -96,6 +97,14 @@ export class Watch {
           try {
             await this.saveTxRawToCache([tx]);
             // return await bulkCreateTransaction(ctx, [tx]);
+            const chainId: any = tx.chainId;
+            const hash = chainId == "SN_MAIN" ? starknetHashFormat(tx.hash) : tx.hash;
+            const count = (await prodDb.query(`select count(1) from transaction where hash="${hash}"`))[0][0]["count(1)"];
+            if (count) {
+              console.log(`${chainId} ${hash} is success in mainnet DB ${count}`);
+              return;
+            }
+            ctx.logger.info(`handle hash ${chainId} ${hash}`);
             await bulkCreateTransaction(ctx, [tx]);
           } catch (error) {
           }
